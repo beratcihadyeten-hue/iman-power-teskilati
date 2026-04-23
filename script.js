a1ce21305e83bcf0128eb330b3c4d2da319fd9e7
beratcihadyeten-hue <beratcihadyeten@gmail.com>
2026-04-23 14:31:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function checkLogin() {
-    const user = document.getElementById('username').value;
+    const user = document.getElementById('username').value.toLowerCase();
     const pass = document.getElementById('password').value;
 
-    // Buraya kendi şifrelerini ekle
-    if ((user === "admin" && pass === "ipt2026") || (user === "zeki" && pass === "aslan")) {
+    // Teşkilat Üyeleri
+    const database = {
+        "admin": "12345",
+        "zeki": "aslan",
+        "berat": "ipt2026"
+    };
+
+    if (database[user] && database[user] === pass) {
         document.getElementById('login-screen').style.display = 'none';
-        document.getElementById('main-panel').style.display = 'block';
+        document.getElementById('main-panel').style.display = 'flex';
+        document.getElementById('display-name').innerText = "OPERATÖR: " + user.toUpperCase();
     } else {
-        alert("YETKİSİZ GİRİŞ ENGELLENDİ!");
+        alert("YETKİSİZ ERİŞİM! Teşkilat dışı giriş denemesi kaydedildi.");
     }
 }
